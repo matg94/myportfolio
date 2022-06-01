1df62f13cc83ec9e08482945df8709a54f777d38
mat <mat.gautron@gmail.com>
2022-06-01 16:15:33 +0100

    Setup with Google Analytics 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import MainPage from './pages/mainPage.jsx'
-import ReactGA from 'react-ga';
-
-const { GA_ID } = process.env;
-ReactGA.initialize(GA_ID);
 
 function App() {
+
   return (
       <MainPage></MainPage>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,21 @@
 import SocialMedia from '../components/SocialMedia'
+import GA4React from 'ga-4-react';
+
+const { GA_ID } = process.env;
+const ga4react = new GA4React(
+    GA_ID,
+    { /* ga custom config, optional */ },
+    [ /* additional code, optional */ ],
+    5000 /* timeout, optional, defaults is 5000 */,
+  );
+    
+ga4react.initialize().then((ga4) => {
+    ga4.pageview('MainPage')
+    ga4.gtag('event','pageview','MainPage') // or your custom gtag event
+},(err) => {
+    console.error(err)
+})
+
 
 function HomePage(props) {
     return (
